b4390b6e18225025df98b1dfbaef273301710dc1
瀧野礼衣 <takiyareii@takiyareiinoMacBook-Pro.local>
2026-07-08 11:38:24 +0900

    曲リストを筐体の外に絶対配置

--- js/script.js
+++ js/script.js
@@ -532,20 +532,9 @@ function startAlbum(tracks){
     generateQR();
 
     // applyMobileLayout();   // スマホなら横一列レイアウトに並べ替え
+    screen.classList.add('show');
     history.pushState({ album: true }, '');
   };
-  // 完成メッセージをふわっと表示（QRから来たときは出さない）
-  const toscreen.classList.add('show');ast = document.getElementById('as-toast');
-    if (toast) {
-    toast.classList.remove('hide');
-    setTimeout(() => toast.classList.add('show'), 200);
-    const hideToast = () => { toast.classList.remove('show'); toast.classList.add('hide'); };
-    toast.onclick = hideToast;
-    let ty0 = 0;
-    toast.ontouchstart = e => { ty0 = e.touches[0].clientY; };
-    toast.ontouchmove  = e => { if (Math.abs(e.touches[0].clientY - ty0) > 20) hideToast(); };
-    setTimeout(hideToast, 5000);
-  }
 
   function closeAlbum(){
     stopPlay();
